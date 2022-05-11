3848332b2ae661c7edcf3fb23e5ed5d32f490f2c
Jason McCoy <jwmccoy29@gmail.com>
2022-05-11 08:10:53 -0500

    user model schema

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const db = require('./db')
 const logger = require('morgan')
+const bodyParser = require('body-parser')
 const countryController = require('./controller/CountryController')
 const cityController = require('./controller/CityController')
 const airlineController = require('./controller/AirlineController')
@@ -10,6 +11,7 @@ const app = express()
 
 const PORT = process.env.PORT || 3001
 
+app.use(bodyParser.json())
 app.use(express.json())
 app.use(cors())
 app.use(logger('dev'))
