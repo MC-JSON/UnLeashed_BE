88ce5db50b506fc3333d5be11f55e5cefdf21264
Jason McCoy <jwmccoy29@gmail.com>
2022-05-11 08:34:10 -0500

    fixed spelling errors and add error func

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -3,8 +3,35 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const { createJWT } = require('../utils/auth')
 
+const emailRegexp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+
 const signup = (req, res, next) => {
   let { name, email, password, password_confirmation } = req.body
+  let errors = []
+  if (!name) {
+    errors.push({ name: 'required' })
+  }
+  if (!email) {
+    errors.push({ email: 'required' })
+  }
+  if (!emailRegexp.test(email)) {
+    errors.push({ email: 'invalid' })
+  }
+  if (!password) {
+    errors.push({ password: 'required' })
+  }
+  if (!password_confirmation) {
+    errors.push({
+      password_confirmation: 'required'
+    })
+  }
+  if (password != password_confirmation) {
+    errors.push({ password: 'mismatch' })
+  }
+  if (errors.length > 0) {
+    return res.status(422).json({ errors: errors })
+  }
   User.findOne({ email: email })
     .then((user) => {
       if (user) {
@@ -47,6 +74,19 @@ const signup = (req, res, next) => {
 
 const signin = (req, res) => {
   let { email, password } = req.body
+  let errors = []
+  if (!email) {
+    errors.push({ email: 'required' })
+  }
+  if (!emailRegexp.test(email)) {
+    errors.push({ email: 'invalid email' })
+  }
+  if (!password) {
+    errors.push({ passowrd: 'required' })
+  }
+  if (errors.length > 0) {
+    return res.status(422).json({ errors: errors })
+  }
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
@@ -68,7 +108,7 @@ const signin = (req, res) => {
               process.env.TOKEN_SECRET,
               (err, decoded) => {
                 if (err) {
-                  res.status(500).json({ erros: err })
+                  res.status(500).json({ errors: err })
                 }
                 if (decoded) {
                   return res.status(200).json({
@@ -81,12 +121,12 @@ const signin = (req, res) => {
             )
           })
           .catch((err) => {
-            res.status(500).json({ erros: err })
+            res.status(500).json({ errors: err })
           })
       }
     })
     .catch((err) => {
-      res.status(500).json({ erros: err })
+      res.status(500).json({ errors: err })
     })
 }
 
